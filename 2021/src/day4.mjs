b838c719a4abe541ba49778f1935b2a4699e2c09
Chris Meyers <chris@chrismeyers.net>
2021-12-03 23:56:37 -0700

    Added 2021 4.1 solution

---
+++ 2021/src/day4.mjs
@@ -0,0 +1,65 @@
+export const solve1 = (input) => {
+  const ROWS = 5;
+  const COLUMNS = 5;
+  const items = input.split(/\n\s*\n/);
+
+  const numbers = items[0].split(',').map((n) => parseInt(n, 10));
+  const boards = items.slice(1).map((board) =>
+    board
+      .trim()
+      .replace(/\s+/g, ' ')
+      .split(' ')
+      .map((number) => ({ value: parseInt(number, 10), marked: false }))
+  );
+
+  let winner;
+  let number;
+  for (let i = 0; i < numbers.length; i++) {
+    if (winner) break;
+    number = numbers[i];
+    for (let j = 0; j < boards.length; j++) {
+      const board = boards[j];
+      for (let k = 0; k < board.length; k++) {
+        const item = board[k];
+        if (item.value === number) item.marked = true;
+      }
+    }
+    for (let j = 0; j < boards.length; j++) {
+      if (winner) break;
+      const board = boards[j];
+      // Horizontal win
+      for (let k = 0; k < COLUMNS; k++) {
+        if (
+          board[k * COLUMNS].marked &&
+          board[k * COLUMNS + 1].marked &&
+          board[k * COLUMNS + 2].marked &&
+          board[k * COLUMNS + 3].marked &&
+          board[k * COLUMNS + 4].marked
+        ) {
+          winner = board;
+        }
+        // Vertical win
+        if (
+          board[k * ROWS].marked &&
+          board[k + 1 * ROWS].marked &&
+          board[k + 2 * ROWS].marked &&
+          board[k + 3 * ROWS].marked &&
+          board[k + 4 * ROWS].marked
+        ) {
+          winner = board;
+        }
+      }
+    }
+  }
+
+  const value = winner.reduce(
+    (acc, item) => acc + (item.marked ? 0 : item.value),
+    0
+  );
+
+  return value * number;
+};
+
+export const solve2 = (input) => {
+  return input;
+};
