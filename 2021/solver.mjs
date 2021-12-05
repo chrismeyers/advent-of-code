c9a9f52d7a010562fa9ed04dfa6e9b5101549eea
Chris Meyers <chris@chrismeyers.net>
2021-12-04 21:42:58 -0700

    Refactored solver program

--- 2021/solver.mjs
+++ 2021/solver.mjs
@@ -10,23 +10,28 @@ if (process.argv.length < 3) {
 
 const day = parseInt(process.argv[2], 10);
 const part = parseInt(process.argv[3], 10);
+
+let solve1;
+let solve2;
 try {
-  const { solve1, solve2 } = await import(
+  ({ solve1, solve2 } = await import(
     `${dirname(import.meta.url)}/src/day${day}.mjs`
-  );
-
-  const inputFile = `${dirname(import.meta.url)}/input/day${day}.txt`;
-  let input;
-  if (fs.existsSync(inputFile)) {
-    input = fs.readFileSync(inputFile, { encoding: 'utf-8' }).trim();
-  }
-
-  if (Number.isNaN(part) || part === 1) {
-    console.log(solve1(input));
-  }
-  if (Number.isNaN(part) || part === 2) {
-    console.log(solve2(input));
-  }
+  ));
 } catch (error) {
   console.error(error.message);
+  process.exit(1);
+}
+
+const inputFile = `${dirname(import.meta.url)}/input/day${day}.txt`;
+if (!fs.existsSync(inputFile)) {
+  console.error(`Input file '${inputFile}' does not exist`);
+  process.exit(1);
+}
+const input = fs.readFileSync(inputFile, { encoding: 'utf-8' }).trim();
+
+if (Number.isNaN(part) || part === 1) {
+  console.log(solve1(input));
+}
+if (Number.isNaN(part) || part === 2) {
+  console.log(solve2(input));
 }
