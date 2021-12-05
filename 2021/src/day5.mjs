0e9e3f6117c821bab66e2d50ebe5e60a3807f258
Chris Meyers <chris@chrismeyers.net>
2021-12-05 00:19:10 -0700

    Simplified map `set()` calls in 5.1 solution

--- 2021/src/day5.mjs
+++ 2021/src/day5.mjs
@@ -10,54 +10,30 @@ export const solve1 = (input) => {
     const [[x1, y1], [x2, y2]] = items[i];
 
     if (x1 === x2) {
-      points.set(
-        `${x1},${y1}`,
-        points.get(`${x1},${y1}`) ? points.get(`${x1},${y1}`) + 1 : 1
-      );
+      points.set(`${x1},${y1}`, (points.get(`${x1},${y1}`) ?? 0) + 1);
       if (y1 < y2) {
         for (let j = y1 + 1; j < y2; j++) {
-          points.set(
-            `${x1},${j}`,
-            points.get(`${x1},${j}`) ? points.get(`${x1},${j}`) + 1 : 1
-          );
+          points.set(`${x1},${j}`, (points.get(`${x1},${j}`) ?? 0) + 1);
         }
       } else if (y1 > y2) {
         for (let j = y1 - 1; j > y2; j--) {
-          points.set(
-            `${x1},${j}`,
-            points.get(`${x1},${j}`) ? points.get(`${x1},${j}`) + 1 : 1
-          );
+          points.set(`${x1},${j}`, (points.get(`${x1},${j}`) ?? 0) + 1);
         }
       }
-      points.set(
-        `${x1},${y2}`,
-        points.get(`${x1},${y2}`) ? points.get(`${x1},${y2}`) + 1 : 1
-      );
+      points.set(`${x1},${y2}`, (points.get(`${x1},${y2}`) ?? 0) + 1);
     }
     if (y1 === y2) {
-      points.set(
-        `${x1},${y1}`,
-        points.get(`${x1},${y1}`) ? points.get(`${x1},${y1}`) + 1 : 1
-      );
+      points.set(`${x1},${y1}`, (points.get(`${x1},${y1}`) ?? 0) + 1);
       if (x1 < x2) {
         for (let j = x1 + 1; j < x2; j++) {
-          points.set(
-            `${j},${y1}`,
-            points.get(`${j},${y1}`) ? points.get(`${j},${y1}`) + 1 : 1
-          );
+          points.set(`${j},${y1}`, (points.get(`${j},${y1}`) ?? 0) + 1);
         }
       } else if (x1 > x2) {
         for (let j = x1 - 1; j > x2; j--) {
-          points.set(
-            `${j},${y1}`,
-            points.get(`${j},${y1}`) ? points.get(`${j},${y1}`) + 1 : 1
-          );
+          points.set(`${j},${y1}`, (points.get(`${j},${y1}`) ?? 0) + 1);
         }
       }
-      points.set(
-        `${x2},${y1}`,
-        points.get(`${x2},${y1}`) ? points.get(`${x2},${y1}`) + 1 : 1
-      );
+      points.set(`${x2},${y1}`, (points.get(`${x2},${y1}`) ?? 0) + 1);
     }
   }
 
