e6f0ca4d3a9b0a420a1076deeb28ae58e3e3b8c0
Chris Meyers <chris@chrismeyers.net>
2021-12-11 00:21:30 -0700

    Added 2021 11.1 solution

---
+++ 2021/src/day11.mjs
@@ -0,0 +1,117 @@
+export const solve1 = (input) => {
+  const items = input.split('\n').map((item) => item.split('').map(Number));
+
+  const columns = items[0].length;
+  const rows = items.length;
+  let flashes = 0;
+  let flashed;
+
+  const flash = (x, y) => {
+    // Left
+    if (x - 1 >= 0 && !flashed[y][x - 1]) {
+      items[y][x - 1] += 1;
+      if (items[y][x - 1] > 9) {
+        items[y][x - 1] = 0;
+        flashed[y][x - 1] = true;
+        flashes += 1;
+        flash(x - 1, y);
+      }
+    }
+    // Right
+    if (x + 1 <= columns - 1 && !flashed[y][x + 1]) {
+      items[y][x + 1] += 1;
+      if (items[y][x + 1] > 9) {
+        items[y][x + 1] = 0;
+        flashed[y][x + 1] = true;
+        flashes += 1;
+        flash(x + 1, y);
+      }
+    }
+    // Up
+    if (y - 1 >= 0 && !flashed[y - 1][x]) {
+      items[y - 1][x] += 1;
+      if (items[y - 1][x] > 9) {
+        items[y - 1][x] = 0;
+        flashed[y - 1][x] = true;
+        flashes += 1;
+        flash(x, y - 1);
+      }
+    }
+    // Down
+    if (y + 1 <= rows - 1 && !flashed[y + 1][x]) {
+      items[y + 1][x] += 1;
+      if (items[y + 1][x] > 9) {
+        items[y + 1][x] = 0;
+        flashed[y + 1][x] = true;
+        flashes += 1;
+        flash(x, y + 1);
+      }
+    }
+    // Up left
+    if (y - 1 >= 0 && x - 1 >= 0 && !flashed[y - 1][x - 1]) {
+      items[y - 1][x - 1] += 1;
+      if (items[y - 1][x - 1] > 9) {
+        items[y - 1][x - 1] = 0;
+        flashed[y - 1][x - 1] = true;
+        flashes += 1;
+        flash(x - 1, y - 1);
+      }
+    }
+    // Up right
+    if (y - 1 >= 0 && x + 1 <= columns - 1 && !flashed[y - 1][x + 1]) {
+      items[y - 1][x + 1] += 1;
+      if (items[y - 1][x + 1] > 9) {
+        items[y - 1][x + 1] = 0;
+        flashed[y - 1][x + 1] = true;
+        flashes += 1;
+        flash(x + 1, y - 1);
+      }
+    }
+    // Down left
+    if (y + 1 <= rows - 1 && x - 1 >= 0 && !flashed[y + 1][x - 1]) {
+      items[y + 1][x - 1] += 1;
+      if (items[y + 1][x - 1] > 9) {
+        items[y + 1][x - 1] = 0;
+        flashed[y + 1][x - 1] = true;
+        flashes += 1;
+        flash(x - 1, y + 1);
+      }
+    }
+    // Down right
+    if (y + 1 <= rows - 1 && x + 1 <= columns - 1 && !flashed[y + 1][x + 1]) {
+      items[y + 1][x + 1] += 1;
+      if (items[y + 1][x + 1] > 9) {
+        items[y + 1][x + 1] = 0;
+        flashed[y + 1][x + 1] = true;
+        flashes += 1;
+        flash(x + 1, y + 1);
+      }
+    }
+  };
+
+  for (let i = 0; i < 100; i++) {
+    flashed = new Array(rows);
+    for (let ii = 0; ii < flashed.length; ii++) {
+      flashed[ii] = new Array(columns).fill(false);
+    }
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < columns; x++) {
+        if (!flashed[y][x]) {
+          items[y][x] += 1;
+          if (items[y][x] > 9) {
+            items[y][x] = 0;
+            flashed[y][x] = true;
+            flashes += 1;
+            flash(x, y);
+          }
+        }
+      }
+    }
+  }
+
+  return flashes;
+};
+
+export const solve2 = (input) => {
+  return input;
+};
---
+++ 2021/test/day11.mjs
@@ -0,0 +1,37 @@
+import fs from 'node:fs';
+import tap from 'tap';
+import { solve1, solve2 } from '../src/day11.mjs';
+import { dirname } from '../util.mjs';
+
+tap.test('works with example input', (t) => {
+  const input = `
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
+`.trim();
+
+  t.equal(solve1(input), 1656);
+  t.equal(solve2(input), input);
+
+  t.end();
+});
+
+tap.test('works with puzzle input', (t) => {
+  const input = fs
+    .readFileSync(`${dirname(import.meta.url)}/../input/day11.txt`, {
+      encoding: 'utf-8',
+    })
+    .trim();
+
+  t.equal(solve1(input), 1632);
+  t.equal(solve2(input), input);
+
+  t.end();
+});
