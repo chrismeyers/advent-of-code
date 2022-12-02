a8a8e9992773535bcccbc773c2624817efd9dcfd
Chris Meyers <chris@chrismeyers.net>
2022-12-01 21:29:07 -0700

    Fixed solution function parameter names

--- 2022/generate.py
+++ 2022/generate.py
@@ -39,11 +39,11 @@ if __name__ == "__main__":
         f.write(
             "\n".join(
                 [
-                    "def part1(input):",
+                    "def part1(data):",
                     "    return 0",
                     "",
                     "",
-                    "def part2(input):",
+                    "def part2(data):",
                     "    return 0",
                     "",
                 ]
