64d91ea0397cedc5bf19a589f9fae6216ad2e8de
Chris Meyers <chris@chrismeyers.net>
2022-12-02 13:23:52 -0700

    Moved test fixture loading into setUpClass

--- 2022/day01/test.py
+++ 2022/day01/test.py
@@ -2,16 +2,18 @@ import os
 import unittest
 from day01.solutions import part1, part2
 
-dir = os.path.dirname(os.path.realpath(__file__))
-with open(f"{dir}/sample.txt", "r") as f:
-    sample_data = f.read()
-with open(f"{dir}/input.txt", "r") as f:
-    input_data = f.read()
-
 
 class TestDay01Solutions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        dir = os.path.dirname(os.path.realpath(__file__))
+        with open(f"{dir}/sample.txt", "r") as f:
+            cls.sample_data = f.read()
+        with open(f"{dir}/input.txt", "r") as f:
+            cls.input_data = f.read()
+
     def test_part1_sample(self):
-        self.assertEqual(part1(sample_data), 24000)
+        self.assertEqual(part1(self.sample_data), 24000)
 
     def test_part1_sample_different_order(self):
         self.assertEqual(
@@ -38,13 +40,13 @@ class TestDay01Solutions(unittest.TestCase):
         )
 
     def test_part1_input(self):
-        self.assertEqual(part1(input_data), 69883)
+        self.assertEqual(part1(self.input_data), 69883)
 
     def test_part2_sample(self):
-        self.assertEqual(part2(sample_data), 45000)
+        self.assertEqual(part2(self.sample_data), 45000)
 
     def test_part2_input(self):
-        self.assertEqual(part2(input_data), 207576)
+        self.assertEqual(part2(self.input_data), 207576)
 
 
 if __name__ == "__main__":
--- 2022/day02/test.py
+++ 2022/day02/test.py
@@ -2,25 +2,27 @@ import os
 import unittest
 from day02.solutions import part1, part2
 
-dir = os.path.dirname(os.path.realpath(__file__))
-with open(f"{dir}/sample.txt", "r") as f:
-    sample_data = f.read()
-with open(f"{dir}/input.txt", "r") as f:
-    input_data = f.read()
-
 
 class TestDay02Solutions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        dir = os.path.dirname(os.path.realpath(__file__))
+        with open(f"{dir}/sample.txt", "r") as f:
+            cls.sample_data = f.read()
+        with open(f"{dir}/input.txt", "r") as f:
+            cls.input_data = f.read()
+
     def test_part1_sample(self):
-        self.assertEqual(part1(sample_data), 15)
+        self.assertEqual(part1(self.sample_data), 15)
 
     def test_part1_input(self):
-        self.assertEqual(part1(input_data), 10404)
+        self.assertEqual(part1(self.input_data), 10404)
 
     def test_part2_sample(self):
-        self.assertEqual(part2(sample_data), 12)
+        self.assertEqual(part2(self.sample_data), 12)
 
     def test_part2_input(self):
-        self.assertEqual(part2(input_data), 10334)
+        self.assertEqual(part2(self.input_data), 10334)
 
 
 if __name__ == "__main__":
