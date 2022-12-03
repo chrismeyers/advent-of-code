6a2a3d40d80706d6536f17ed51dd311197b96883
Chris Meyers <chris@chrismeyers.net>
2022-12-03 10:16:04 -0700

    Simplified 2022.3 solutions

--- 2022/day03/solutions.py
+++ 2022/day03/solutions.py
@@ -7,13 +7,10 @@ def part1(data):
 
     for item in data:
         compartment1, compartment2 = item[: len(item) // 2], item[len(item) // 2 :]
-        intersections = list(set(compartment1) & set(compartment2))
-        for chars in intersections:
-            for char in chars:
-                if char.isupper():
-                    sum += ord(char) - uppercase_offset
-                else:
-                    sum += ord(char) - lowercase_offset
+        intersection = list(set(compartment1) & set(compartment2))[0]
+        sum += ord(intersection) - (
+            uppercase_offset if intersection.isupper() else lowercase_offset
+        )
 
     return sum
 
@@ -31,9 +28,8 @@ def part2(data):
 
     for group1, group2, group3 in groups:
         intersection = list(set(group1) & set(group2) & set(group3))[0]
-        if intersection.isupper():
-            sum += ord(intersection) - uppercase_offset
-        else:
-            sum += ord(intersection) - lowercase_offset
+        sum += ord(intersection) - (
+            uppercase_offset if intersection.isupper() else lowercase_offset
+        )
 
     return sum
