f23ed98349ec14b2f1c64cfb707d8664c0020a1e
Chris Meyers <chris@chrismeyers.net>
2022-12-06 08:08:08 -0700

    Renamed variable in 2022.6

--- 2022/day06/solutions.py
+++ 2022/day06/solutions.py
@@ -2,17 +2,17 @@ from collections import Counter
 
 
 def _solve(data, num_distinct):
-    position = 0
+    chars_processed = 0
 
     for i, _ in enumerate(data):
         current = data[i : i + num_distinct]
         counts = Counter(current)
 
         if len(counts) == num_distinct:
-            position = i + num_distinct
+            chars_processed = i + num_distinct
             break
 
-    return position
+    return chars_processed
 
 
 def part1(data):
