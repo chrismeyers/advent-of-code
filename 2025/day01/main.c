5a8aef2400ed9490559f70235afd8c1bc1b89f03
Chris Meyers <chris@chrismeyers.net>
2025-12-01 20:26:07 -0700

    Add 2025 1.2 solution

--- 2025/day01/main.c
+++ 2025/day01/main.c
@@ -57,6 +57,34 @@ int part1(char *input)
     return zeros;
 }
 
+// Sample: 6
+// Input: 6860
+int part2(char *input)
+{
+    int zeros = 0;
+    int loc = 50;
+    char *line;
+
+    line = strtok(input, "\n");
+
+    while (line != NULL) {
+        char direction = line[0];
+        int value = atoi(&line[1]);
+
+        int target = direction == 'R' ? loc + value : loc - value;
+        do {
+            loc = direction == 'R' ? loc + 1 : loc - 1;
+            if (wrap(loc, 0, 99) == 0) zeros++;
+        } while (loc != target);
+
+        loc = wrap(target, 0, 99);
+
+        line = strtok(NULL, "\n");
+    }
+
+    return zeros;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
@@ -70,8 +98,22 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("%d\n", part1(buffer));
+    char *part1_buffer = malloc(strlen(buffer) + 1);
+    char *part2_buffer = malloc(strlen(buffer) + 1);
+    if (part1_buffer == NULL || part2_buffer == NULL) {
+        fprintf(stderr, "Error allocating memory\n");
+        free(buffer);
+        return 1;
+    }
+
+    memcpy(part1_buffer, buffer, strlen(buffer) + 1);
+    memcpy(part2_buffer, buffer, strlen(buffer) + 1);
+
+    printf("%d\n", part1(part1_buffer));
+    printf("%d\n", part2(part2_buffer));
 
     free(buffer);
+    free(part1_buffer);
+    free(part2_buffer);
     return 0;
 }
